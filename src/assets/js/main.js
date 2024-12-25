da0faf405831b0a78a90bcfd68d60b1b2716ea67
mamiko <nicotto1016@gmail.com>
2024-12-25 12:04:38 +0900

    gnavのマークアップ

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -12,6 +12,7 @@ const target_options = {
   arrows: false,
   interval: 7000, //1枚のスライドをどのぐらい表示させておくか（次のスライド表示まで何秒かけるか。）⇒Swiperのautoplayのdelayに相当
   speed: 2000, //次のスライダーへの移動時間をミリ秒単位で指定（ミリ秒）
+  pagination: false,
 };
 
 const mySplide = new Splide(target, target_options);
